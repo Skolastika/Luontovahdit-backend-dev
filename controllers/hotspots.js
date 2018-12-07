f2a5ab4fce76713c7918788593038b2a609e91d8
Henna-Riikka Ruonala <hennariikka.ruonala@gmail.com>
2018-12-08 01:33:49 +0200

    Added delete and patch for hotspotsRouter + comment model

--- controllers/hotspots.js
+++ controllers/hotspots.js
@@ -34,5 +34,56 @@ hotspotsRouter.post('/', async (request, response) => {
   }
 })
 
+hotspotsRouter.delete('/:id', async (request, response) => {
+  try {
+    const hotspot = await Hotspot.findById(request.params.id)
+    if (!hotspot) {
+      return response.status(204).end()
+    }
+    // should check user token here
+    await hotspot.remove()
+    response.status(204).end()
+  } catch (exception) {
+    console.log(exception)
+    if (exception.kind === 'ObjectId') {
+      return response.status(400).json({ error: 'Malformed id.' })
+    }
+    response.status(500).json({ error: 'Failed to delete hotspot.' })
+  }
+})
+
+hotspotsRouter.patch('/:id', async (request, response) => {
+  try {
+    /*
+    Tank.findByIdAndUpdate(id, { $set: { size: 'large' }}, { new: true }, function (err, tank) {
+      if (err) return handleError(err);
+      res.send(tank);
+    });*/
+
+
+    const hotspot = await Hotspot.findById(request.params.id)
+    const body = request.body
+    if (hotspot) {
+      if (body._id) {
+        delete body._id
+      }
+      console.log(body)
+      for (let a in body) {
+        hotspot[a] = body[a]
+      }
+      hotspot.save((error, updatedHotspot) => {
+        if (error) {
+          const paths = Object.keys(error.errors)
+          return response.status(400).json({ error: `Validation error: problem with ${paths.join(', ')}.` })
+        }
+        return response.status(200).json(updatedHotspot)
+      })
+    }
+  } catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'Failed to update hotspot.' })
+  }
+})
+
 
 module.exports = hotspotsRouter
---
+++ models/comment.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+mongoose.plugin(require("mongoose-ajv-plugin"))
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    pictureUrl: {
+      type: String,
+      'ajv-schema': {
+        type: 'string',
+        format: 'url'
+      }
+    },
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    inHotspot: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Hotspot'
+    },
+    upVotes: {
+      type: Number,
+      default: 0
+    },
+    downVotes: {
+      type: Number,
+      default: 0
+    },
+    flagged: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+)
+
+const Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment
